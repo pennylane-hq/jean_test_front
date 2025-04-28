52722dc9215a0a2d450c333baceacff9fce065c2
Valentin Bastien <valentin.bastien@pennylane.com>
2025-04-28 16:37:00 +0200

    prettier file

--- src/app/NoApiTokenAlert.tsx
+++ src/app/NoApiTokenAlert.tsx
@@ -1,5 +1,9 @@
 const NoApiTokenAlert = () => {
-  return <div className="alert alert-danger mt-5 mb-3">No API token found, set it in your .env.local file</div>
+  return (
+    <div className="alert alert-danger mt-5 mb-3">
+      No API token found, set it in your .env.local file
+    </div>
+  )
 }
 
 export default NoApiTokenAlert
