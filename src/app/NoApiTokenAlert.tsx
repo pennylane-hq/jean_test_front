854e04fa191faa7dca6a43358a49697e23a69fff
Valentin Bastien <valentin.bastien@pennylane.com>
2025-04-28 14:35:47 +0200

    Remove token

---
+++ src/app/NoApiTokenAlert.tsx
@@ -0,0 +1,5 @@
+const NoApiTokenAlert = () => {
+  return <div className="alert alert-danger mt-5 mb-3">No API token found, set it in your .env.local file</div>
+}
+
+export default NoApiTokenAlert
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,17 +4,24 @@ import App from './App'
 import { ApiProvider } from '../api'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
+import NoApiTokenAlert from './NoApiTokenAlert';
 
 const domRoot = document.getElementById('root');
 const root = createRoot(domRoot!);
 
+console.log(process.env.REACT_APP_API_TOKEN)
+
 root.render(
   <React.StrictMode>
-    <ApiProvider
-      url="https://jean-test-api.herokuapp.com/"
-      token="" // set your api token here
-    >
-      <App />
-    </ApiProvider>
+    {process.env.REACT_APP_API_TOKEN ? (
+      <ApiProvider
+        url="https://jean-test-api.herokuapp.com/"
+        token={process.env.REACT_APP_API_TOKEN ?? ''}
+      >
+        <App />
+      </ApiProvider>
+    ) : (
+      <NoApiTokenAlert />
+    )}
   </React.StrictMode>
 )
